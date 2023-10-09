a8e39d39c49dc38e78257fcfd01e8bb7b7c1ff1f
IdaHaliti <idahaliti24@gmail.com>
2023-10-09 11:14:47 +0200

    displaying images fetched from the api

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,23 @@
-"use strict";
+const dataUrl = "https://dummyjson.com/products";
+async function fetchData() {
+    try {
+        const response = await fetch(dataUrl);
+        const fetchedData = await response.json();
+        return fetchedData.products;
+    }
+    catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+const data = await fetchData();
+console.log(data);
+const main = document.querySelector('#main-container');
+for (let i = 0; i < 5; i++) {
+    main.innerHTML +=
+        `<div class="image--container">
+        <img class="image__thumbnail" src="${data && data[i].thumbnail}" alt="${data && data[i].brand}">
+      </div>`;
+}
+export {};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,41 @@
-import { Product } from "./Product";
-import { fetchData } from "./fetchData";
-
+export interface Product {
+  id: number,
+  title: string,
+  description: string,
+  brand: string,
+  category: string,
+  discountPercentage: number,
+  images: string[],
+  price: string,
+  rating: number,
+  stock: number,
+  thumbnail: string,
+}
 
 const dataUrl = "https://dummyjson.com/products"
-const data : Product[] | null = await fetchData(dataUrl);
 
+ async function fetchData(): Promise<Product[] | null> {
+    try {
+        const response = await fetch(dataUrl);
+        const fetchedData = await response.json()
+
+        return fetchedData.products;
+    } catch (err) {
+        console.log(err);
+        return null
+    }
+
+}
+
+
+const data : Product[] | null =await fetchData();
+console.log(data)
+
+const main = document.querySelector<HTMLDivElement>('#main-container') 
+
+for (let i = 0; i < 5; i++) {
+    main!.innerHTML += 
+    `<div class="image--container">
+        <img class="image__thumbnail" src="${data && data[i].thumbnail}" alt="${data && data[i].brand}">
+      </div>`
+}
